50fb4efd42a13c4e9dc104272e2ee10ac8f07c78
conformist-mw <conformist.mw@gmail.com>
2017-07-29 22:29:02 +0300

    added main_page app for index page

--- python_ru/urls.py
+++ python_ru/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from main_page.views import MainPage
 
 
 urlpatterns = [
+    url(r'^$', MainPage.as_view()),
     url(r'^admin/', admin.site.urls),
     url(r'^api/cards/', include('cards_api.urls')),
     url(r'^articles/', include('articles.urls')),
-    ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
